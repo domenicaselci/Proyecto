4aa7b5e115a9fdfe5122d7a44cdfd2d3fe0bec44
domenicaselci <domenicaselci2002@gmail.com>
2021-09-02 13:47:42 -0300

    prueba.txt

---
+++ app.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+//console.log(fs);
+
+let datos = fs.readFileSync(__dirname + "/prueba.txt", "utf-8");
+console.log(datos)
+let moment = require("moment");
+console.log(moment().format("MMM Do YYYY"));
